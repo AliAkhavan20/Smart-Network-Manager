38d94b493a9074357ad4565195c6af65d89e23ff
arazhunter <ali_akhavan20@yahoo.com>
2019-04-28 13:32:48 +0430

    done

--- Microsoft Scripts/ActiveDirectory/Add Users/Addusers.ps1
+++ Microsoft Scripts/ActiveDirectory/Add Users/Addusers.ps1
@@ -5,13 +5,14 @@
 
 #Make users out of users List
 
-$userlist = import-csv -Path "Please enter destination source \\\ users list.csv" -Encoding UTF8
+$userlist = import-csv -Path "I:\pws\My pws\Smart-Network-Manager\Microsoft Scripts\ActiveDirectory\Add Users\users list.csv" -Encoding UTF8
 
 foreach ( $user in $userlist ) {
-    $Des = # write down your description 
-    New-ADOrganizationalUnit -name $item -Description $Des -Path "OU=920000,OU=Domain Objects,DC=PowerShell,DC=Local"
 
+$Des = # write down your description 
+New-ADOrganizationalUnit -ComputerName $user  -Description $Des -Path "OU=920000,OU=Domain Objects,DC=PowerShell,DC=Local"
 Invoke-Command -ComputerName 'dc' -ScriptBlock { ipconfig }
+
 
 }
 
